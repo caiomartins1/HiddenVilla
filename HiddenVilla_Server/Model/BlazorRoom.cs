59bc72c909fd7974ca62076f998a1255359031bf
Caio Martins <dev.caiomartins@gmail.com>
2021-08-11 17:13:01 +0100

    One way data binding

---
+++ HiddenVilla_Server/Model/BlazorRoom.cs
@@ -0,0 +1,13 @@
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+
+        public string RoomName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool isActive { get; set; }
+    }
+}
