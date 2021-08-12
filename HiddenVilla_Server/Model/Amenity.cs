b043819061cc2932169ff76e80986e1c98849b29
Caio Martins <dev.caiomartins@gmail.com>
2021-08-12 18:01:16 +0100

    Assignment 4

---
+++ HiddenVilla_Server/Model/Amenity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HiddenVilla_Server.Model
+{
+    public class Amenity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
